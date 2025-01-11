a9393cf86e3a51dad6f7b4e5ef45b0e1f6208af0
Oscar <osm1019@gmail.com>
2025-01-10 20:14:08 -0800

    caimito: MistOS

--- lineage_tokay.mk
+++ lineage_tokay.mk
@@ -16,6 +16,27 @@ $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 $(call inherit-product, device/google/zumapro/lineage_common.mk)
 $(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
 
+#MistOS Extras
+# UDFPS support 
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+# Face Unlock
+TARGET_FACE_UNLOCK := false
+# Blur
+TARGET_ENABLE_BLUR := true
+# BCR (Call recording)
+TARGET_PREBUILT_BCR := true
+#TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+# Lawnchair (Pixel launcher by default)
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+# GMS
+WITH_GMS := true
+# Mist OS Flags
+MIST_BUILD_TYPE := OFFICIAL
+MISTOS_MAINTAINER := OscarM
+
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9
