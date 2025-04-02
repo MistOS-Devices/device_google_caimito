87870b19afaf9fdbafac5ee2675c80fdee6ac1d0
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-04-02 17:40:03 -0400

    Partial bring-up for almost all custom ROMs

--- lineage_caiman.mk
+++ lineage_caiman.mk
@@ -5,7 +5,12 @@
 #
 
 # Inherit some common stuff
+ROM_VENDOR := lineage
+ifdef ROM_VENDOR
+$(call inherit-product, vendor/$(ROM_VENDOR)/config/common_full_phone.mk)
+else
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+endif
 
 # Inherit device configuration
 DEVICE_CODENAME := caiman
@@ -16,11 +21,16 @@ $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 9 Pro
+ifdef ROM_VENDOR
+PRODUCT_NAME := $(ROM_VENDOR)_$(DEVICE_CODENAME)
+else
 PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+endif
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2856
 TARGET_SCREEN_WIDTH := 1280
+TARGET_BOOT_ANIMATION_RES := 1280
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="caiman-user 16 BP4A.260205.002 14624737 release-keys" \
