afd048999e5f774fb9d60bc75eee150512babcd0
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-08-29 16:13:10 -0400

    Project Mist OS official bring-up

--- device-caiman.mk
+++ device-caiman.mk
@@ -127,6 +127,8 @@ include hardware/google/pixel/powershare/device.mk
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/vendor.prop
+# Custom MistOS about phone properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/custom_system.prop
 
 # Satellite
 PRODUCT_COPY_FILES += \
